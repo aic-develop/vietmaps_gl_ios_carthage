c70fe53b3558f04a907a053129b03a6695ae270a
phu2810 <nguyenphu2810@gmail.com>
2020-05-12 08:09:37 +0700

    update

---
+++ VietMapsGLCarthage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VietMapsGLCarthage
+//
+//  Created by Phu on 5/12/20.
+//  Copyright © 2020 VietMaps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ VietMapsGLCarthage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VietMapsGLCarthage
+//
+//  Created by Phu on 5/12/20.
+//  Copyright © 2020 VietMaps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
