d1ae7bd6dfda9f7939c9770c6d5b5de30ef7aedb
phu2810 <nguyenphu2810@gmail.com>
2020-05-12 08:43:57 +0700

    update scheme

---
+++ VietMaps/VietMaps.h
@@ -0,0 +1,19 @@
+//
+//  VietMaps.h
+//  VietMaps
+//
+//  Created by Phu on 5/12/20.
+//  Copyright © 2020 VietMaps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VietMaps.
+FOUNDATION_EXPORT double VietMapsVersionNumber;
+
+//! Project version string for VietMaps.
+FOUNDATION_EXPORT const unsigned char VietMapsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VietMaps/PublicHeader.h>
+
+
