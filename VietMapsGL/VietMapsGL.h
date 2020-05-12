ded194e4660d7160743edc7f553d645df86085d9
phu2810 <nguyenphu2810@gmail.com>
2020-05-12 08:29:48 +0700

    add cartfile

---
+++ VietMapsGL/VietMapsGL.h
@@ -0,0 +1,19 @@
+//
+//  VietMapsGL.h
+//  VietMapsGL
+//
+//  Created by Phu on 5/12/20.
+//  Copyright © 2020 VietMaps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VietMapsGL.
+FOUNDATION_EXPORT double VietMapsGLVersionNumber;
+
+//! Project version string for VietMapsGL.
+FOUNDATION_EXPORT const unsigned char VietMapsGLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VietMapsGL/PublicHeader.h>
+
+
